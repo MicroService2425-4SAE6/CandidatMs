d5250e58145f91f11c90fbe648e6ccffc6624332
Oumeima-IbnElfekih <oumeima.ibnelfekih@esprit.tn>
2025-02-13 09:48:10 +0100

    add eureka config

--- src/main/java/com/candidatsae/candidatsae6/CandidatSae6Application.java
+++ src/main/java/com/candidatsae/candidatsae6/CandidatSae6Application.java
@@ -4,9 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
-
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CandidatSae6Application {
 
     public static void main(String[] args) {
